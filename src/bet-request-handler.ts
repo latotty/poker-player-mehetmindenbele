1f6fd4c958c102b6547ff086f7c4873f6b95a5a1
LaTotty <latotty91@gmail.com>
2019-12-20 11:45:21 +0100

    chore: readability

--- src/bet-request-handler.ts
+++ src/bet-request-handler.ts
@@ -1,8 +1,7 @@
 import { GameState } from './types';
 
 export const handleBetRequestFactory: () => (_: GameState) => Promise<number> = () => async gameState => {
-  return (
-    Math.max(0, gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise + 1) ||
-    0
-  );
+  const ourPlayer = gameState.players[gameState.in_action];
+
+  return Math.max(0, gameState.current_buy_in - ourPlayer.bet + gameState.minimum_raise + 1) || 0;
 };
