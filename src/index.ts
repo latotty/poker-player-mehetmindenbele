ea973642fe5495e5f63892ee08d30e3cbef480ea
LaTotty <latotty91@gmail.com>
2019-12-20 14:30:49 +0100

    chore: better log

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,13 @@ app.post('/', (req, res) => {
   try {
     if (req.body.action === 'bet_request') {
       const bet = handleBetRequest(JSON.parse(req.body.game_state));
-      console.log(JSON.stringify({ bet_request: req.body, bet }));
+      console.log({ bet_request: req.body, bet });
       res.status(200).send(bet.toString());
       return;
     }
     if (req.body.action === 'showdown') {
       handleShowdown(JSON.parse(req.body.game_state));
-      console.log(JSON.stringify({ showdown: req.body }));
+      console.log({ showdown: req.body });
       res.status(200).send('OK');
       return;
     }
