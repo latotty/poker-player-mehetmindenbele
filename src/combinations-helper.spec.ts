41d00ab428a83223a5107c01045a9c6150ccc82c
LaTotty <latotty91@gmail.com>
2019-12-20 14:30:30 +0100

    test: poker, 2pair, 3kind

--- src/combinations-helper.spec.ts
+++ src/combinations-helper.spec.ts
@@ -27,6 +27,45 @@ const cases: {
     ],
     score: 1,
   },
+  {
+    hand: [
+      { rank: '7', suit: 'hearts' },
+      { rank: 'K', suit: 'clubs' },
+    ],
+    comm: [
+      { rank: '9', suit: 'hearts' },
+      { rank: '10', suit: 'clubs' },
+      { rank: 'K', suit: 'spades' },
+      { rank: '7', suit: 'clubs' },
+    ],
+    score: 2,
+  },
+  {
+    hand: [
+      { rank: '5', suit: 'spades' },
+      { rank: '5', suit: 'diamonds' },
+    ],
+    comm: [
+      { rank: '9', suit: 'hearts' },
+      { rank: '10', suit: 'clubs' },
+      { rank: '10', suit: 'spades' },
+      { rank: '5', suit: 'clubs' },
+    ],
+    score: 3,
+  },
+  {
+    hand: [
+      { rank: '5', suit: 'spades' },
+      { rank: '5', suit: 'diamonds' },
+    ],
+    comm: [
+      { rank: '5', suit: 'hearts' },
+      { rank: '10', suit: 'clubs' },
+      { rank: '10', suit: 'spades' },
+      { rank: '5', suit: 'clubs' },
+    ],
+    score: 4,
+  },
 ];
 
 cases.forEach(({ score, hand, comm }, i) =>
