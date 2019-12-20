767ae9d911011162f5114626c8ac0d47fddbb555
LaTotty <latotty91@gmail.com>
2019-12-20 15:01:05 +0100

    feat: straight & straight flush

--- src/combinations-helper.spec.ts
+++ src/combinations-helper.spec.ts
@@ -55,6 +55,19 @@ const cases: {
   },
   {
     hand: [
+      { rank: '4', suit: 'spades' },
+      { rank: '5', suit: 'diamonds' },
+    ],
+    comm: [
+      { rank: '6', suit: 'hearts' },
+      { rank: '7', suit: 'clubs' },
+      { rank: '8', suit: 'spades' },
+      { rank: '5', suit: 'clubs' },
+    ],
+    score: 4,
+  },
+  {
+    hand: [
       { rank: '5', suit: 'spades' },
       { rank: '5', suit: 'diamonds' },
     ],
@@ -64,7 +77,20 @@ const cases: {
       { rank: '10', suit: 'spades' },
       { rank: '5', suit: 'clubs' },
     ],
-    score: 4,
+    score: 7,
+  },
+  {
+    hand: [
+      { rank: '4', suit: 'diamonds' },
+      { rank: '5', suit: 'diamonds' },
+    ],
+    comm: [
+      { rank: '6', suit: 'diamonds' },
+      { rank: '7', suit: 'diamonds' },
+      { rank: '8', suit: 'diamonds' },
+      { rank: '5', suit: 'clubs' },
+    ],
+    score: 8,
   },
 ];
 
--- src/combinations-helper.ts
+++ src/combinations-helper.ts
@@ -14,14 +14,51 @@ const isTwoPair: CardMatcher = (hand, comm) =>
 const isThreeOfAKind: CardMatcher = (hand, comm) =>
   hand.filter((c, i) => [...hand.slice(i + 1), ...comm].filter(c2 => c.rank === c2.rank).length >= 2).length >= 1;
 
+const straights: Card['rank'][][] = [
+  ['A', '2', '3', '4', '5'],
+  ['2', '3', '4', '5', '6'],
+  ['3', '4', '5', '6', '7'],
+  ['4', '5', '6', '7', '8'],
+  ['5', '6', '7', '8', '9'],
+  ['6', '7', '8', '9', '10'],
+  ['7', '8', '9', '10', 'J'],
+  ['8', '9', '10', 'J', 'Q'],
+  ['9', '10', 'J', 'Q', 'K'],
+  ['10', 'J', 'Q', 'K', 'A'],
+];
+
+const suites = ['clubs', 'spades', 'hearts', 'diamonds'];
+
+const isStraight: CardMatcher = (hand, comm) =>
+  straights.some(
+    sRanks =>
+      // MUST be at least one card in hand
+      sRanks.some(sRank => hand.some(c => c.rank === sRank)) &&
+      // MUST be every card in hand + comm
+      sRanks.every(sRank => [...hand, ...comm].some(c => c.rank === sRank)),
+  );
+
 const isFourOfAKind: CardMatcher = (hand, comm) =>
   hand.filter((c, i) => [...hand.slice(i + 1), ...comm].filter(c2 => c.rank === c2.rank).length >= 3).length >= 1;
+
+const isStraightFlush: CardMatcher = (hand, comm) =>
+  suites.some(suite =>
+    straights.some(
+      sRanks =>
+        // MUST be at least one card in hand
+        sRanks.some(sRank => hand.some(c => c.rank === sRank && c.suit === suite)) &&
+        // MUST be every card in hand + comm
+        sRanks.every(sRank => [...hand, ...comm].some(c => c.rank === sRank && c.suit === suite)),
+    ),
+  );
 
 const matchers: [number, CardMatcher][] = [
   [1, isOnePair],
   [2, isTwoPair],
   [3, isThreeOfAKind],
-  [4, isFourOfAKind],
+  [4, isStraight],
+  [7, isFourOfAKind],
+  [8, isStraightFlush],
 ];
 
 export const checkCombinations = (...opts: Parameters<CardMatcher>): number =>
