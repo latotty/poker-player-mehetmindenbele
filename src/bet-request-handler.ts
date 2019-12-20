6d4c8f8b6fe2710a3f5a9b8897cf918cdafff602
Dávid Juhász <jdavid@codingsans.com>
2019-12-20 11:18:44 +0100

    feat: minimum raise

--- src/bet-request-handler.ts
+++ src/bet-request-handler.ts
@@ -1,5 +1,11 @@
 import { GameState } from './types';
 
-export const handleBetRequestFactory: () => (_: GameState) => Promise<number> = () => async () => {
-  return 0;
+export const handleBetRequestFactory: () => (_: GameState) => Promise<number> = () => async ({
+  current_buy_in,
+  players,
+  in_action,
+  bet_index,
+  minimum_raise,
+}) => {
+  return current_buy_in - players[in_action][bet_index] + minimum_raise + 1;
 };
