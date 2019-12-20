2194ff9b6d5b982e8ac9ed4ea90659811ab602f6
LaTotty <latotty91@gmail.com>
2019-12-20 11:33:05 +0100

    chore: finished type

--- src/types.ts
+++ src/types.ts
@@ -73,7 +73,7 @@ export interface Player {
 }
 export interface Holecard {
   /** Rank of the card. Possible values are numbers 2-10 and J,Q,K,A */
-  rank: string;
+  rank: '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
   /** Suit of the card. Possible values are: clubs,spades,hearts,diamonds */
   suit: 'clubs' | 'spades' | 'hearts' | 'diamonds';
 }
